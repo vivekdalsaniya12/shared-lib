90aace020024f107583049634e9dadba4d53245e
Dalsaniya Vivek <168078146+vivekdalsaniya12@users.noreply.github.com>
2025-05-17 11:03:07 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,7 @@
-def call(Map x){
+def call(String USERNAME, String PASSWORD, String DOCKER_IMAGE, String IMAGE_TAG){
   sh """
-      echo ${x.PASSWORD} | docker login -u ${x.USERNAME} --password-stdin
-      docker push ${x.DOCKER_IMAGE}:${x.IMAGE_TAG}
+      echo PASSWORD | docker login -u USERNAME --password-stdin
+      docker push DOCKER_IMAGE:IMAGE_TAG
       docker logout
   """
 }
